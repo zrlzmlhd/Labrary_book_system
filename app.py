519d71823209c91687f0eb1fc42fdbd28cddcee4
Li Tian Xian <sofialeyva771996exo@gmail.com>
2025-12-17 09:59:49 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+from src.pipeline.predict_pipeine import PredictPipeline, CustomData
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+app = application
+
+# 创建一个主页
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predictdata",methods= ["GET","POST"])
+def predict_datapoint():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        data = CustomData(
+            gender=request.form.get("gender"),
+            race_ethnicity=request.form.get("race_ethnicity"),
+            parental_level_of_education=request.form.get("parental_level_of_education"),
+            lunch=request.form.get("lunch"),
+            test_preparation_course=request.form.get("test_preparation_course"),
+            reading_score=float(request.form.get("reading_score")),
+            writing_score=float(request.form.get("writing_score"))
+        )
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+
+        predict_pipeline = PredictPipeline()
+        results=predict_pipeline.predict(pred_df)
+
+        return render_template("home.html", results=results[0])
+if __name__ == "__main__":
+    print("请在浏览器中访问: http://127.0.0.1:5002")
+    app.run(debug=True,host="0.0.0.0",port=5002)
